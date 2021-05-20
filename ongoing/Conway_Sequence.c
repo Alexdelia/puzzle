97d0846caf53489643eeb747c6ce7c0d28e16e4c
Alexdelia <alexandre.delille.57@gmail.com>
2021-05-20 23:07:27 +0200

    solving Conway Sequence

---
+++ ongoing/Conway_Sequence.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+/*
+**	Codingame Puzzle
+*/
+
+int *ft_conway_sequence()
+
+int main()
+{
+    int R;
+    scanf("%d", &R);
+    int L;
+    scanf("%d", &L);
+
+    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
+    // To debug: fprintf(stderr, "Debug messages...\n");
+
+    printf("answer\n");
+
+    return 0;
+}
