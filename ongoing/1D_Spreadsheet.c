10eee923e70b1c0eb5d60a7ce723ab125fe29f4b
Alexdelia <alexandre.delille.57@gmail.com>
2021-05-26 20:09:00 +0200

    change type

--- ongoing/1D_Spreadsheet.c
+++ ongoing/1D_Spreadsheet.c
@@ -25,44 +25,44 @@ typedef struct  s_1d
 t_1d    **ft_resolve(int size, t_1d *d[size], int index)
 {
     // look if it has been solved before
-    if (d[index].s == TRUE)
+    if (d[index]->s == TRUE)
         return (d);
     
     // convert a1
-    if (d[index].a1[0] == '$')
+    if (d[index]->a1[0] == '$')
     {
         // do recursion to solve d[$x]
-        d = ft_resolve(size, d, atoi(&d[index].a1[1]));
-        d[index].i1 = d[atoi(&d[index].a1[1])].res;
+        d = ft_resolve(size, d, atoi(&d[index]->a1[1]));
+        d[index]->i1 = d[atoi(&d[index]->a1[1])]->res;
     }
     else
-        d[index].i1 = atoi(&d[index].a1[1]);
+        d[index]->i1 = atoi(&d[index]->a1[1]);
     // convert a2
-    if (d[index].a2[0] == '$')
+    if (d[index]->a2[0] == '$')
     {
         // do recursion to solve d[$x]
-        d = ft_resolve(size, d, atoi(&d[index].a2[1]));
-        d[index].i2 = d[atoi(&d[index].a2[1])].res;
+        d = ft_resolve(size, d, atoi(&d[index]->a2[1]));
+        d[index]->i2 = d[atoi(&d[index]->a2[1])]->res;
     }    
     else
-        d[index].i2 = atoi(&d[index].a2[1]);
+        d[index]->i2 = atoi(&d[index]->a2[1]);
     
 
     // convert res with VALUE operator
-    if (strcmp(d[index].op, "VALUE") == 0)
-        d[index].res = d[index].i1;
+    if (strcmp(d[index]->op, "VALUE") == 0)
+        d[index]->res = d[index]->i1;
     // convert res with ADD operator
-    else if (strcmp(d[index].op, "ADD") == 0)
-        d[index].res = d[index].i1 + d[index].i2;
+    else if (strcmp(d[index]->op, "ADD") == 0)
+        d[index]->res = d[index]->i1 + d[index]->i2;
     // convert res with SUB operator
-    else if (strcmp(d[index].op, "SUB") == 0)
-        d[index].res = d[index].i1 - d[index].i2;
+    else if (strcmp(d[index]->op, "SUB") == 0)
+        d[index]->res = d[index]->i1 - d[index]->i2;
     // convert res with MULT operator
-    else if (strcmp(d[index].op, "MULT") == 0)
-        d[index].res = d[index].i1 * d[index].i2;
+    else if (strcmp(d[index]->op, "MULT") == 0)
+        d[index]->res = d[index]->i1 * d[index]->i2;
     
     // change solved as TRUE
-    d[index].s = TRUE;
+    d[index]->s = TRUE;
     return (d);
 }
 
