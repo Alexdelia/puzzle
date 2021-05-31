e62989d9378e480f3479b263d8596a01ac125f30
Alexdelia <alexandre.delille.57@gmail.com>
2021-05-31 19:51:24 +0200

    solved sentence_formater

--- solved/Sentence_Formater/srcs/ft_output.c
+++ solved/Sentence_Formater/srcs/ft_output.c
@@ -1,19 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_output.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/05/31 17:06:54 by adelille          #+#    #+#             */
-/*   Updated: 2021/05/31 17:07:53 by adelille         ###   ########.fr       */
+/*   Created: 2021/05/31 18:52:13 by adelille          #+#    #+#             */
+/*   Updated: 2021/05/31 19:03:47 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fo.h"
 
-int	main(int ac, char **av)
+void	ft_output(t_arg *a)
 {
-	// blank for the moment
-	return (0);
+	int	fd;
+
+	fd = STDOUT;
+	if (a->file == TRUE)
+	{
+		if ((fd = open(a->path, O_WRONLY | O_TRUNC | O_CREAT, 0664)) <= 0)
+		{
+			ft_pserc("Error: Unable to open/write/trunc/create ", RED);
+			ft_pserc(a->path, RED);
+			ft_pser("\n");
+			return ;
+		}
+	}
+	ft_putstr_fd(a->res, fd);
+	write(fd, "\n", 1);
+	if (fd != STDOUT)
+		close(fd);
 }
