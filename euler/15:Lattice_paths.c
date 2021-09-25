7518f322cddfc0b5f81c884e608c9e9c39cb26de
Alexdelia <alexandre.delille.57@gmail.com>
2021-09-25 08:38:31 +0200

    improve

--- euler/15:Lattice_paths.c
+++ euler/15:Lattice_paths.c
@@ -1,16 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   15:.c                                              :+:      :+:    :+:   */
+/*   15:Lattice_paths.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 20:28:50 by adelille          #+#    #+#             */
-/*   Updated: 2021/09/24 20:36:46 by adelille         ###   ########.fr       */
+/*   Updated: 2021/09/25 08:38:12 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+
+#define	SIDE	21
 
 // https://en.wikipedia.org/wiki/Central_binomial_coefficient
 // https://www.youtube.com/watch?v=gMlf1ELvRzc
@@ -18,15 +20,15 @@
 int	main(void)
 {
 
-	long	grid[20][20];
+	long	grid[SIDE][SIDE];
 	int		x;
 	int		y;
 
 	x = 0;
-	while (x < 20)
+	while (x < SIDE)
 	{
 		y = 0;
-		while (y < 20)
+		while (y < SIDE)
 		{
 			if (y == 0 || x == 0)
 				grid[x][y] = 1;
@@ -38,6 +40,6 @@ int	main(void)
 		printf("\n");
 		x++;
 	}
-	printf("Possible route for 20x20: %ld\n", grid[19][19]);
+	printf("Possible route for %dx%d: %ld\n", SIDE - 1, SIDE - 1, grid[SIDE - 1][SIDE - 1]);
 	return (0);
 }
