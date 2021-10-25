ebb0ba5aef7ae73ca1202853e5588b52a317bd0b
Alexdelia <alexandre.delille.57@gmail.com>
2021-10-25 15:42:33 +0200

    fix 24

--- euler/24:Lexicographic_permutations.c
+++ euler/24:Lexicographic_permutations.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/23 12:57:03 by adelille          #+#    #+#             */
-/*   Updated: 2021/10/23 13:56:14 by adelille         ###   ########.fr       */
+/*   Updated: 2021/10/25 15:40:30 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,20 +39,25 @@ static int	ft_is_dec(char n[SIZE], int i)
 
 static int	ft_smallest_after(char n[SIZE], int i)
 {
-	int	s;
-	int	index;
+	int		s;
+	int		index;
+	int		x;
 
 	s = SIZE;
 	index = 0;
-	while (i < SIZE)
+	x = i;
+	i++;
+	while (i < SIZE - 1 && n[i])
 	{
-		if (n[i] - 48 < s)
+		if (n[i] - 48 < s && n[i] > n[x])
 		{
 			s = n[i] - 48;
 			index = i;
 		}
 		i++;
 	}
+	if (s == SIZE)
+		return (x - 1);
 	return (index);
 }
 
@@ -78,7 +83,7 @@ static void	ft_permutation(char n[SIZE])
 	while (i < SIZE && ft_is_dec(n, i) == FALSE)
 		i++;
 	x = i - 1;
-	i = ft_smallest_after(n, i - 1);
+	i = ft_smallest_after(n, x);
 	ft_swap(n, x, i);
 	ft_permute(n, x + 1);
 }
@@ -88,11 +93,13 @@ int	main(void)
 	char	n[SIZE] = {"0123456798"};
 	int		i;
 
-	i = 1;
-	while (i == 1000000 || i < 20)
+	//printf("%s\n", n);
+	i = 2;
+	while (i < 1000000)
 	{
 		ft_permutation(n);
 		i++;
+		//printf("%s\n", n);
 	}
 	printf("%s\n", n);
 	return (0);
