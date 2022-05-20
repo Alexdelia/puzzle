8aaaf67d9f723cac34e4c086c68bffc92fe654c8
Alexdelia <alexandre.delille.57@gmail.com>
2022-05-20 10:43:07 +0200

    redo

--- solved/Temperature.c
+++ solved/Temperature.c
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/11 18:57:17 by adelille          #+#    #+#             */
-/*   Updated: 2021/05/11 18:57:48 by adelille         ###   ########.fr       */
+/*   Updated: 2022/05/20 10:42:56 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,54 +19,20 @@
 #include <string.h>
 #include <stdbool.h>
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
-
-int main()
+int	main(void)
 {
-    // the number of temperatures to analyse
-    int n;
-    int index;
-    scanf("%d", &n);
-    float tmp;
-    int degree[n];
-    float closer;
-    int closer_i;
+	int	n, c, i, t;
 
-    index = 0;
-    for (int i = 0; i < n; i++) {
-        // a temperature expressed as an integer ranging from -273 to 5526
-        int t;
-        scanf("%d", &t);
-        degree[i] = t;
-    }
-    closer_i = 0;
-    if (degree[0] < 0)
-        closer = -degree[0];
-    else
-        closer = degree[0];
-    while (index < n)
-    {
-        if (degree[index] < 0)
-            tmp = -degree[index] + 0.5;
-        else
-            tmp = degree[index];
-        fprintf(stderr, "degree = %d\ttrans = %f\n", degree[index], tmp);
-        fprintf(stderr, "closer = %f\ttrans[%d] = %f\n\n", closer, index, tmp);
-        if (closer > tmp)
-        {
-            closer = tmp;
-            closer_i = index;
-        }
-        index++;
-    }
-
-    // Write an answer using printf(). DON'T FORGET THE TRAILING \n
-    // To debug: fprintf(stderr, "Debug messages...\n");
-
-    printf("%d\n", degree[closer_i]);
-
-    return 0;
+	scanf("%d", &n);
+	c = (n > 0 ? 10001 : 0);
+	while (i < n)
+	{
+		scanf("%d", &t);
+		c = abs(t) < abs(c) ? t : c;
+		if (abs(t) == abs(c) && t > c)
+			c = t;
+		i++;
+	}
+	printf("%d\n", c);
+	return (0);
 }
