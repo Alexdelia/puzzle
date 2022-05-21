16f1ce5886d3de227f478445fde944ccc853c027
Alexdelia <alexandre.delille.57@gmail.com>
2022-05-21 10:16:08 +0200

    Temperature.py

---
+++ codingame/solved/Temperature.py
@@ -0,0 +1,29 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Temperature.py                                     :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: adelille <adelille@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2022/05/21 10:15:49 by adelille          #+#    #+#              #
+#    Updated: 2022/05/21 10:15:56 by adelille         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+n = int(input())  # the number of temperatures to analyse
+c = n * 9999
+for i in input().split():
+    # t: a temperature expressed as an integer ranging from -273 to 5526
+    t = int(i)
+    if abs(t) < abs(c):
+        c = t
+    if abs(t) == abs(c) and t > c:
+        c = t
+
+print(c)
