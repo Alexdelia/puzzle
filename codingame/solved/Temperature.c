f5f52852f3a8d3a9f52b916470fb663f859bbff6
Alexdelia <alexandre.delille.57@gmail.com>
2022-05-21 10:09:42 +0200

    reput Temperature.c and add .cpp

---
+++ codingame/solved/Temperature.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Temperature.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/11 18:57:17 by adelille          #+#    #+#             */
+/*   Updated: 2022/05/21 10:09:03 by adelille         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+**	Codingame Puzzle
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int	main(void)
+{
+	int	n, c, i, t;
+
+	scanf("%d", &n);
+	c = (n > 0 ? 10001 : 0);
+	while (i < n)
+	{
+		scanf("%d", &t);
+		c = abs(t) < abs(c) ? t : c;
+		if (abs(t) == abs(c) && t > c)
+			c = t;
+		i++;
+	}
+	printf("%d\n", c);
+	return (0);
+}
