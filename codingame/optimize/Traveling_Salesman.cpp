b7e4ddada6c94c6efa4eaf95a6122805802f65b9
Alexdelia <alexandre.delille.57@gmail.com>
2022-07-03 19:21:19 +0200

    optimize more

--- codingame/optimize/Traveling_Salesman.cpp
+++ codingame/optimize/Traveling_Salesman.cpp
@@ -6,7 +6,7 @@
 /*   By: adelille <adelille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 15:54:06 by adelille          #+#    #+#             */
-/*   Updated: 2022/07/03 18:55:31 by adelille         ###   ########.fr       */
+/*   Updated: 2022/07/03 19:19:20 by adelille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@
 #include <vector>
 #include <cmath>
 #include <bits/stdc++.h>
-
-#define DEBUG	1
 
 #define DEPTH	1
 
@@ -37,15 +35,12 @@ std::map<std::pair<t_index, t_index>, float>	ad;	// all distance
 std::vector<t_index>							p;	// final path
 float											best_d;	// best distance
 
-static float	distance(const t_index one, const t_index two)
+static const float	distance(const t_index one, const t_index two)
 {
-	return (sqrtf(powf((m[two].x - m[one].x), 2) + powf((m[two].y - m[one].y), 2)));
+	if (ad.find(std::make_pair(one, two)) == ad.end())
+		ad.insert(std::make_pair(std::make_pair(one, two), sqrtf(powf((m[two].x - m[one].x), 2) + powf((m[two].y - m[one].y), 2))));
+	return (ad[std::make_pair(one, two)]);
 }
-
-/*static float	distance(const int x1, const int y1, const int x2, const int y2)
-{
-	return (sqrtf(powf((x2 - x1), 2) + powf((y2 - y1), 2)));
-}*/
 
 static const std::map<float, int>	&distance(const t_index curr, std::map<float, int> &dist)
 {
@@ -54,57 +49,25 @@ static const std::map<float, int>	&distance(const t_index curr, std::map<float, 
 	while (i < m.size())
 	{
 		if (m[i].visited == false)
-			dist[ad[std::make_pair(curr, i)]] = i;
+			dist[distance(curr, i)] = i;
 		i++;
 	}
 
 	return (dist);
 }
 
-static void	all_distance(void)
-{
-	t_index			one;
-	t_index			two;
-	const t_index	size = m.size();
-
-	one = 0;
-	while (one < size)
-	{
-		two = 0;
-		while (two < size)
-		{
-			ad[std::make_pair(one, two)] = distance(one, two);
-			two++;
-		}
-		one++;
-	}	
-}
-
 static void	solve(const t_index curr, const int node, const float t, std::vector<t_index> &path)
 {
-	/*if (DEBUG)
-	{
-		std::cerr << "> ";
-		for (std::vector<t_index>::const_iterator i = path.begin(); i != path.end(); ++i)
-			std::cerr << *i << ' ';
-		std::cerr << '\t' << t << '\t' << node << '\t' << curr << std::endl;
-	}*/
 
 	if (t >= best_d)
 		return ;
 
 	if (node == 0)
 	{
-		if (t + ad[std::make_pair(curr, 0)] < best_d)
+		if (t + distance(curr, 0) < best_d)
 		{
-			best_d = t + ad[std::make_pair(curr, 0)];
+			best_d = t + distance(curr, 0);
 			p = path;
-			if (DEBUG)
-			{
-				for (std::vector<t_index>::const_iterator i = p.begin(); i != p.end(); ++i)
-					std::cerr << *i << ' ';
-				std::cerr << "0\t" << best_d << std::endl;
-			}
 		}
 		return ;
 	}
@@ -136,7 +99,6 @@ int	main(void)
 	}
 
 	best_d = 2000 * n;
-	all_distance();
 
 	std::vector<t_index>	path(1, 0);
 	m[0].visited = true;
