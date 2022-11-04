931883ec28d0d6731aa458550b72a8c88ee9ff88
Alexdelia <alexandre.delille.57@gmail.com>
2022-11-04 17:18:49 +0100

    silver

--- codingame/bot/Mad_Pod_Racing.py
+++ codingame/bot/Mad_Pod_Racing.py
@@ -1,60 +1,72 @@
 import math
 import sys
-from typing import Tuple, Union
+from typing import List, Tuple, Union
 
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
-T_ANGLE = 45
+T_ANGLE = 15
 B_ANGLE = 90
 T_ANGLE_SPEED = 1.0
-B_ANGLE_SPEED = 0.5
+B_ANGLE_SPEED = 0.0
 
-T_DIST = 4000
-B_DIST = 0
+T_DIST = 1200
+B_DIST = 600
 T_DIST_SPEED = 1.0
 B_DIST_SPEED = 0.1
 
 TILT = 1000
 
+H_DRIFT = 3
+
+
 class Env:
-    x: int
-    y: int
-    next_x: int     # x position of the next check point
-    next_y: int     # y position of the next check point
+    c: Tuple[int, int] = (-1, -1)
+    prev: Tuple[int, int]
+    checkpoint: Tuple[int, int]
+    opponent: Tuple[int, int]
     dist: int       # distance to the next checkpoint
     angle: int      # angle between your pod orientation and the direction of the next checkpoint
-    opponent_x: int
-    opponent_y: int
-
+    check: List[Tuple[int, int]] = []
+    drift: float = 0
+    speed: float = 0
 
     def get_info(self):
-        self.x, self.y, self.next_x, self.next_y, self.dist, self.angle = [int(i) for i in input().split()]
-        self.opponent_x, self.opponent_y = [int(i) for i in input().split()]
-    
+        x, y, next_x, next_y, self.dist, self.angle = [
+            int(i) for i in input().split()]
+        self.prev = [self.c, (x, y)][self.c == (-1, -1)]
+        self.c = (x, y)
+        self.speed = math.sqrt((self.c[0] - self.prev[0]) ** 2 +
+                               (self.c[1] - self.prev[1]) ** 2)
+        self.drift = -H_DRIFT * self.speed
+        self.checkpoint = (next_x, next_y)
+        if self.checkpoint not in self.check:
+            self.check.append(self.checkpoint)
+        op_x, op_y = [int(i) for i in input().split()]
+        self.opponent = (op_x, op_y)
 
     def get_thrust(self) -> Union[int, str]:
         if self.angle > 90 or self.angle < -90:
             return 0
-        elif abs(self.angle) < 3 and self.dist > T_DIST + 2000 \
-                and abs(self.x - self.opponent_x) > 1500 and abs(self.y - self.opponent_y) > 1500:
-            print(f"diff: {abs(self.x - self.opponent_x)} {abs(self.y - self.opponent_y)}", file=sys.stderr)
-            print(f"angle: {self.angle}", file=sys.stderr)
+        elif abs(self.angle) < 3 and self.dist > 8000 \
+                and abs(self.c[0] - self.opponent[0]) > 1500 and abs(self.c[1] - self.opponent[1]) > 1500:
             return "BOOST"
 
-        ### Angle
+        # Angle
         # x          | 0   | B   | (B+T)/2 | T  | inf  (abs)
         # self.angle | 0   | 45  | 67.5    | 90 | 180  (abs)
         # s_angle    | 0.5 | 0.5 | 0.75    | 1  | 1
-        if self.angle > B_ANGLE:
+        if abs(self.angle) > B_ANGLE:
             s_angle = B_ANGLE_SPEED
-        elif self.angle < T_ANGLE:
+        elif abs(self.angle) < T_ANGLE:
             s_angle = T_ANGLE_SPEED
         else:
-            s_angle = B_ANGLE_SPEED + (T_ANGLE_SPEED - B_ANGLE_SPEED) * (self.angle - B_ANGLE) / (T_ANGLE - B_ANGLE)
+            s_angle = B_ANGLE_SPEED + \
+                (T_ANGLE_SPEED - B_ANGLE_SPEED) * \
+                (abs(self.angle) - B_ANGLE) / (T_ANGLE - B_ANGLE)
         print(f"s_angle: {s_angle}", file=sys.stderr)
-        
-        ### Distance
+
+        # Distance
         # x          | 0   | B   | (B+T)/2 | T    | inf  (abs)
         # self.dist  | 0   | 500 | 750     | 1000 | inf (abs)
         # s_dist     | 0   | 0   | 0.25    | 1    | 1
@@ -63,33 +75,34 @@ class Env:
         elif self.dist < B_DIST:
             s_dist = B_DIST_SPEED
         else:
-            s_dist = B_DIST_SPEED + (T_DIST_SPEED - B_DIST_SPEED) * (self.dist - B_DIST) / (T_DIST - B_DIST)
+            s_dist = B_DIST_SPEED + \
+                (T_DIST_SPEED - B_DIST_SPEED) * \
+                (self.dist - B_DIST) / (T_DIST - B_DIST)
         print(f"s_dist: {s_dist}", file=sys.stderr)
 
         return int(100 * (s_angle + s_dist) / 2)
-    
 
     def get_targeted_xy(self) -> Tuple[int, int]:
-        if self.dist < 2000 or abs(self.angle) < 15:
-            return self.next_x, self.next_y
-        # isosceles triangle abc, find point c with dist_ab = dist_ac
-        a = (self.x, self.y)
-        b = (self.next_x, self.next_y)
-        dist_ab = math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
-        dist_bc = TILT
-        if self.angle < 0:
-            dist_bc = -dist_bc
-        c = (b[0] + dist_bc * (a[0] - b[0]) / dist_ab, b[1] + dist_bc * (a[1] - b[1]) / dist_ab)
-        return int(c[0]), int(c[1])
+        # depending on drift, apply a correction offset to the target
+        d_x = self.c[0] - self.prev[0]
+        d_y = self.c[1] - self.prev[1]
+        d = math.sqrt(d_x ** 2 + d_y ** 2)
+        if d != 0:
+            d_x /= d
+            d_y /= d
+        return (self.checkpoint[0] + int(self.drift * d_x), self.checkpoint[1] + int(self.drift * d_y))
         # return int(x), int(y)
-    
 
     def debug(self):
-        print(f"I: {self.x}\t{self.y}", file=sys.stderr)
-        print(f"N: {self.next_x}\t{self.next_y}", file=sys.stderr)
+        print(f"I: {self.c}", file=sys.stderr)
+        print(f"P: {self.prev}", file=sys.stderr)
+        print(f"N: {self.checkpoint}", file=sys.stderr)
+        print(f"O: {self.opponent}", file=sys.stderr)
         print(f"D: {self.dist}", file=sys.stderr)
         print(f"A: {self.angle}", file=sys.stderr)
-        print(f"O: {self.opponent_x}\t{self.opponent_y}", file=sys.stderr)
+        print(f"Dft: {self.drift}", file=sys.stderr)
+        print(f"Spd: {self.speed}", file=sys.stderr)
+
 
 e = Env()
 
