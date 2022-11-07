5af99e0494caae89aac5c61c8b319dd71e2ee2f2
Alexdelia <alexandre.delille.57@gmail.com>
2022-11-07 21:13:25 +0100

    new IO

--- codingame/bot/Mad_Pod_Racing.py
+++ codingame/bot/Mad_Pod_Racing.py
@@ -19,53 +19,37 @@ TILT = 1000
 
 H_DRIFT = 3
 
+def distance(x1: int, y1: int, x2: int, y2: int) -> float:
+    return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+
 class Pod:
-    c: Tuple[int, int] = (-1, -1)
-    prev: Tuple[int, int]
-    dist: int
-    angle: int
-    drift: float
-    speed: float
-
-class Env:
-    c: Tuple[int, int] = (-1, -1)
-    prev: Tuple[int, int]
-    checkpoint: Tuple[int, int]
-    opponent: Tuple[int, int]
-    dist: int       # distance to the next checkpoint
-    angle: int      # angle between your pod orientation and the direction of the next checkpoint
-    n_laps: int
-    n_check: int
-    check: List[Tuple[int, int]] = []
-    drift: float = 0
-    speed: float = 0
-
-    def init_info(self):
-        self.n_laps = int(input())
-        self.n_check = int(input())
-        for _ in range(self.n_check):
-            self.check.append(tuple([int(i) for i in input().split()]))
-
-    def get_info(self):
-        x, y, next_x, next_y, self.dist, self.angle = [
-            int(i) for i in input().split()]
-        self.prev = [self.c, (x, y)][self.c == (-1, -1)]
+    def __init__(self, x: int, y: int, vx: int, vy: int, angle: int, next_x: int, next_y: int):
+        self.c: Tuple[int, int] = (x, y)
+        self.s: Tuple[int, int] = (vx, vy)
+        self.prev: Tuple[int, int] = (x - vx, y - vy)
+        self.next_check: Tuple[int, int] = (next_x, next_y)
+        self.dist: float = distance(x, y, next_x, next_y)
+        self.angle: int = angle
+        self.speed: float = distance(self.prev[0], self.prev[1], self.c[0], self.c[1])
+        self.drift: float = -H_DRIFT * self.speed
+    
+    def update(self, x: int, y: int, vx: int, vy: int, angle: int, next_x: int, next_y: int):
+        self.prev = self.c
         self.c = (x, y)
-        self.speed = math.sqrt((self.c[0] - self.prev[0]) ** 2 +
-                               (self.c[1] - self.prev[1]) ** 2)
+        self.s = (vx, vy)
+        self.next_check = (next_x, next_y)
+        self.dist = distance(x, y, next_x, next_y)
+        self.angle = angle
+        self.speed = distance(self.prev[0], self.prev[1], self.c[0], self.c[1])
         self.drift = -H_DRIFT * self.speed
-        self.checkpoint = (next_x, next_y)
-        if self.checkpoint not in self.check:
-            self.check.append(self.checkpoint)
-        op_x, op_y = [int(i) for i in input().split()]
-        self.opponent = (op_x, op_y)
-
-    def get_thrust(self) -> Union[int, str]:
+    
+    def get_thrust(self, opponent: List[Tuple[int, int]]) -> Union[int, str]:
         if self.angle > 90 or self.angle < -90:
             return 0
-        elif abs(self.angle) < 3 and self.dist > 8000 \
-                and abs(self.c[0] - self.opponent[0]) > 1500 and abs(self.c[1] - self.opponent[1]) > 1500:
-            return "BOOST"
+        elif abs(self.angle) < 3 and self.dist > 8000:
+            for o in opponent:
+                if distance(self.c[0], self.c[1], o[0], o[1]) > 2121:
+                    return "BOOST"
 
         # Angle
         # x          | 0   | B   | (B+T)/2 | T  | inf  (abs)
@@ -105,32 +89,70 @@ class Env:
         if d != 0:
             d_x /= d
             d_y /= d
-        return (self.checkpoint[0] + int(self.drift * d_x), self.checkpoint[1] + int(self.drift * d_y))
+        return (self.next_check[0] + int(self.drift * d_x), self.next_check[1] + int(self.drift * d_y))
         # return int(x), int(y)
 
-    def init_debug(self):
-        print(f"n_laps: {self.n_laps}", file=sys.stderr)
-        print(f"n_check: {self.n_check}", file=sys.stderr)
-        print(f"check: {self.check}", file=sys.stderr)
+    def __str__(self) -> str:
+        ret = f"C: {self.c}\n"
+        ret += f" P: {self.prev}\n"
+        ret += f" N: {self.next_check}\n"
+        ret += f" V: {self.s}\n"
+        ret += f" D: {self.dist}\n"
+        ret += f" A: {self.angle}\n"
+        ret += f" Spd: {self.speed}\n"
+        ret += f" Dft: {self.drift}"
+        return ret
 
-    def debug(self):
-        print(f"I: {self.c}", file=sys.stderr)
-        print(f"P: {self.prev}", file=sys.stderr)
-        print(f"N: {self.checkpoint}", file=sys.stderr)
-        print(f"O: {self.opponent}", file=sys.stderr)
-        print(f"D: {self.dist}", file=sys.stderr)
-        print(f"A: {self.angle}", file=sys.stderr)
-        print(f"Dft: {self.drift}", file=sys.stderr)
-        print(f"Spd: {self.speed}", file=sys.stderr)
 
+class Env:
+    bot: List[Pod] = []
+    opponent: List[Pod] = []
+    n_laps: int
+    n_check: int
+    check: List[Tuple[int, int]] = []
+
+    def init_info(self):
+        self.n_laps = int(input())
+        self.n_check = int(input())
+        for _ in range(self.n_check):
+            self.check.append(tuple([int(i) for i in input().split()]))
+
+    def get_info(self, n_pod: int):
+        for i in range(n_pod):
+            x, y, vx, vy, angle, i_check = [int(i) for i in input().split()]
+            if len(self.bot) < n_pod:
+                self.bot.append(Pod(x, y, vx, vy, angle, *self.check[i_check]))
+            else:
+                self.bot[i].update(x, y, vx, vy, angle, *self.check[i_check])
+        for i in range(n_pod):
+            x, y, vx, vy, angle, i_check = [int(i) for i in input().split()]
+            if len(self.opponent) < n_pod:
+                self.opponent.append(Pod(x, y, vx, vy, angle, *self.check[i_check]))
+            else:
+                self.opponent[i].update(x, y, vx, vy, angle, *self.check[i_check])
+
+    def __str__(self) -> str:
+        ret = f"n_laps: {self.n_laps}\n"
+        ret += f"n_check: {self.n_check}\n"
+        ret += f"check: {self.check}\n"
+        ret += "Bot:\n"
+        for b in self.bot:
+            ret += str(b) + "\n"
+        ret += "Opponent:\n"
+        for o in self.opponent:
+            ret += str(o) + "\n"
+        return ret
 
 e = Env()
 e.init_info()
 
 # game loop
 while True:
-    e.get_info()
-    e.debug()
-    thrust = e.get_thrust()
-    x, y = e.get_targeted_xy()
-    print(f"{x} {y} {thrust} {thrust}")
+    e.get_info(2)
+    print(e, file=sys.stderr)
+
+    c_op = [o.c for o in e.opponent]
+    for i, b in enumerate(e.bot):
+        x, y = b.get_targeted_xy()
+        thrust = b.get_thrust(c_op)
+        print(f"{x} {y} {thrust} {thrust}")
