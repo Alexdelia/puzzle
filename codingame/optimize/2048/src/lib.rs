71e6d9dd2989a81edcadf6fe67517c7ac44d660e
Alexdelia <alexandre.delille.57@gmail.com>
2022-11-29 13:43:32 +0100

    sort

---
+++ codingame/optimize/2048/src/lib.rs
@@ -0,0 +1,12 @@
+extern crate enum_like;
+extern crate enum_like_derive;
+extern crate enum_vec;
+
+pub mod game;
+
+pub mod io {
+    pub mod read;
+    pub mod write;
+}
+
+pub mod utils;
--- codingame/optimize/2048/src/search.rs
+++ codingame/optimize/2048/src/search.rs
@@ -1,33 +1,14 @@
-#[path = "2048.rs"]
-mod mod_2048;
-
 use std::collections::HashMap;
-use std::fs::File;
-use std::io::{BufRead, BufReader, ErrorKind, Write};
 use std::process::ExitCode;
 use std::time::{SystemTime, UNIX_EPOCH};
 
-use crate::mod_2048::Board;
-use crate::mod_2048::Move;
-use crate::mod_2048::Score;
-use crate::mod_2048::Seed;
-use crate::mod_2048::R_A;
-use crate::mod_2048::R_C;
-use crate::mod_2048::R_M;
-
-use crate::mod_2048::next;
+use lib2048::err;
+use lib2048::game::{Board, Move, Score, Seed, R_A, R_C, R_M};
+use lib2048::io::{read::read, write::write};
 
 type Games = HashMap<Seed, (Board, Seed, Score)>;
 
 const FILE: &str = ".2048_results.out";
-
-macro_rules! err {
-	($($arg:tt)*) => {
-		eprint!("\x1b[31;1merror\x1b[0m\x1b[1m:\t");
-		eprint!($($arg)*);
-		eprintln!("\x1b[0m");
-	};
-}
 
 fn play(mut board: Board, mut seed: Seed) -> (Vec<Move>, Score) {
     let mut am: Vec<Move>;
@@ -48,75 +29,7 @@ fn play(mut board: Board, mut seed: Seed) -> (Vec<Move>, Score) {
         seed = board.spawn_tile(seed);
     }
 
-    (board.moves, board.score)
-}
-
-fn read() -> Option<Vec<(Seed, Score)>> {
-    let lines: Vec<String> = match File::open(FILE) {
-        Ok(f) => BufReader::new(f).lines().map(|l| l.unwrap()).collect(),
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => {
-                return Some(Vec::new());
-            }
-            _ => {
-                err!("\"\x1b[35m{}\x1b[0m\x1b[1m\" {}", FILE, e);
-                return None;
-            }
-        },
-    };
-
-    let mut ret = Vec::new();
-    for l in lines {
-        let mut s = l.split_whitespace();
-        let seed = s.next().unwrap().parse::<Seed>().unwrap();
-        let score = s.next().unwrap().parse::<Score>().unwrap();
-        ret.push((seed, score));
-    }
-
-    Some(ret)
-}
-
-fn write(seed: Seed, score: Score, moves: &[Move]) -> Option<()> {
-    let mut new_l = String::new();
-    new_l.push_str(&format!("{} {} {} ", seed, next(next(seed)), score));
-    for m in moves {
-        new_l.push_str(&format!("{}", m));
-    }
-
-    let mut lines: Vec<String> = match File::open(FILE) {
-        Ok(f) => BufReader::new(f).lines().map(|l| l.unwrap()).collect(),
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => Vec::new(),
-            _ => {
-                err!("\"\x1b[35m{}\x1b[0m\x1b[1m\" {}", FILE, e);
-                return None;
-            }
-        },
-    };
-
-    let mut found = false;
-    for l in &mut lines {
-        if l.split_whitespace()
-            .next()
-            .unwrap()
-            .parse::<Seed>()
-            .unwrap()
-            == seed
-        {
-            *l = new_l.clone();
-            found = true;
-            break;
-        }
-    }
-
-    if !found {
-        lines.push(new_l);
-    }
-
-    let mut f = File::create(FILE).unwrap();
-    f.write_all(lines.join("\n").as_bytes()).unwrap();
-
-    Some(())
+    (board.moves.into(), board.score)
 }
 
 fn parse() -> Option<Games> {
@@ -154,7 +67,7 @@ fn main() -> ExitCode {
     });
 
     {
-        let bs = read().unwrap_or_else(|| {
+        let bs = read(FILE).unwrap_or_else(|| {
             std::process::exit(2);
         });
 
@@ -173,7 +86,7 @@ fn main() -> ExitCode {
             if new_score > *score {
                 *score = new_score;
                 print!("\x1b[33;1m{}\x1b[0m:\t", seed);
-                write(*seed, *score, &moves);
+                write(FILE, *seed, *score, &moves);
                 println!(
                     "\x1b[32;1m{} \x1b[35;1m{} \x1b[31;1m{}\x1b[0m",
                     *score,
