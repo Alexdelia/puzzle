fbf00a92d718e91a911f7973ed98fb4df912d065
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-01 15:51:35 +0100

    auto template DAY/YEAR

--- Advent_of_Code/src/template.py
+++ Advent_of_Code/src/template.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import re
+from os.path import dirname
+
 from aocd import get_data
 
-DAY = 1
-YEAR = 2022
+DAY = int(re.sub(r'[^0-9]', "", dirname(__file__).split('/')[-1]))
+YEAR = int(re.sub(r'[^0-9]', "", dirname(__file__).split('/')[-2]))
 DATA: str = get_data(day=DAY, year=YEAR)
 
 lines = DATA.splitlines()
