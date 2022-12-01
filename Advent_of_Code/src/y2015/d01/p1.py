065cd2eddc5e05e3e1e28af457ad3bb3684fc70c
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-01 14:55:22 +0100

    y2015/d01

---
+++ Advent_of_Code/src/y2015/d01/p1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from aocd import get_data
+
+DAY = 1
+YEAR = 2015
+DATA: str = get_data(day=DAY, year=YEAR)
+
+r = 0
+bi = 0
+
+for i, c in enumerate(DATA):
+    if c == '(':
+        r += 1
+    elif c == ')':
+        r -= 1
+    if r == -1 and bi == 0:
+        bi = i + 1
+
+print(f"part 1: {r}")
+print(f"part 2: {bi}")
