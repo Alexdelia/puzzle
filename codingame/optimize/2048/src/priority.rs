b8a70328323f678d0ef067cdf1479a05e5544fe7
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-04 10:38:00 +0100

    try

--- codingame/optimize/2048/src/priority.rs
+++ codingame/optimize/2048/src/priority.rs
@@ -1,40 +1,17 @@
 use crate::game::{Board, Cell, Move, SIZE};
 
-type Priority = u32;
+pub type Priority = u32;
 
 pub fn priority(board: &Board) -> Priority {
     let mut r = radix(board);
-    // let mut p: Priority = r[0] as Priority;
-    // for i in r.iter().take(18).skip(1) {
-    //     if i == &1 {
-    //         p += 1;
-    //     }
-    // }
     let mut p: Priority = 0;
-
-    // +1 priority for each cell forming a snake (the snake can be in any direction)
-    // snake:
-    // 15 14 13 12
-    // 8 9 10 11
-    // 7 6 5 4
-    // 0 1 2 3
-    // other snake:
-    // 12 13 14 15
-    // 11 10 9 8
-    // 4 5 6 7
-    // 3 2 1 0
-    // other snake:
-    // 3 2 1 0
-    // 4 5 6 7
-    // 11 10 9 8
-    // 12 13 14 15
     let mut x: usize = 0;
     let mut y: usize = 0;
     let mut dir: Move = Move::Up;
     let mut drift: Move = Move::Up;
     let mut b: bool = true;
 
-    for val in (0..18).rev() {
+    for val in (1..18).rev() {
         while r[val] > 0 {
             if p == 0 {
                 (b, x, y) = is_corner(board, val as Cell);
@@ -56,6 +33,7 @@ pub fn priority(board: &Board) -> Priority {
             break;
         }
     }
+    p += r[0] as Priority;
 
     p * 10_000_000 + board.score
 }
