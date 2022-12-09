bda6a6336b15a69da6b0ad3cb091a0a8077cc1b7
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-09 15:14:04 +0100

    save

---
+++ codingame/optimize/2048/import_to_python.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import re
+
+RESULT = ".2048_results.out"
+ANSWER = "./src/answer.py"
+
+f = open(RESULT, "r")
+out = {}
+for line in f:
+    l = line.split()
+    out[int(l[1])] = l[-1]
+f.close()
+
+start = r"answer = "
+line = start + str(out) + "\n"
+
+f = open(ANSWER, "r")
+lines = f.readlines()
+f.close()
+
+for i, l in enumerate(lines):
+    if l.startswith(start):
+        lines[i] = line
+        break
+
+f = open(ANSWER, "w")
+f.writelines(lines)
+f.close()
