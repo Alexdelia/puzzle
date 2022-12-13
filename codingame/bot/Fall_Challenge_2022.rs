8cc1ea4e9f429c4c2cb66e93946de75cab2151c6
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-13 19:18:53 +0100

    move and build

--- codingame/bot/Fall_Challenge_2022.rs
+++ codingame/bot/Fall_Challenge_2022.rs
@@ -10,7 +10,7 @@ type Scrap = u8;
 type Matter = u32;
 type Unit = u8;
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq, Eq)]
 enum Owner {
     None,
     Me,
@@ -72,6 +72,10 @@ struct Env {
     m_m: Matter,
     o_m: Matter,
     map: Vec<Vec<Tile>>,
+    m_units: Vec<(usize, usize)>,
+    o_units: Vec<(usize, usize)>,
+    m_recycler: Vec<(usize, usize)>,
+    o_recycler: Vec<(usize, usize)>,
 }
 
 impl Env {
@@ -82,6 +86,10 @@ impl Env {
             m_m: 0,
             o_m: 0,
             map: vec![vec![Tile::default(); w]; h],
+            m_units: Vec::new(),
+            o_units: Vec::new(),
+            m_recycler: Vec::new(),
+            o_recycler: Vec::new(),
         }
     }
 
@@ -95,17 +103,92 @@ impl Env {
             self.o_m = parse_input!(inputs[1], Matter);
         }
 
+        self.m_units.clear();
+        self.o_units.clear();
+        self.m_recycler.clear();
+        self.o_recycler.clear();
+
         for x in 0..self.h {
             for y in 0..self.w {
                 let mut input_line = String::new();
                 io::stdin().read_line(&mut input_line).unwrap();
 
                 self.map[x][y].update(&input_line);
+
+                if self.map[x][y].owner == Owner::Me {
+                    for _ in 0..self.map[x][y].unit {
+                        self.m_units.push((x, y));
+                    }
+                    if self.map[x][y].recycler {
+                        self.m_recycler.push((x, y));
+                    }
+                } else if self.map[x][y].owner == Owner::Op {
+                    for _ in 0..self.map[x][y].unit {
+                        self.o_units.push((x, y));
+                    }
+                    if self.map[x][y].recycler {
+                        self.o_recycler.push((x, y));
+                    }
+                }
             }
         }
     }
 
-    fn build(&mut self) {}
+    fn dist(&self, src: (usize, usize), dst: (usize, usize)) -> usize {
+        ((src.0 as isize - dst.0 as isize).abs() + (src.1 as isize - dst.1 as isize).abs()) as usize
+    }
+
+    fn r#move(&mut self, src: (usize, usize), dst: (usize, usize)) {
+        self.map[dst.0][dst.1].owner = Owner::Me;
+        // can build on top of unit, will need to take care
+        // could free tile to build on
+        print!("MOVE 1 {} {} {} {};", src.1, src.0, dst.1, dst.0);
+    }
+
+    fn build(&mut self, pos: (usize, usize)) {
+        self.m_recycler.push(pos);
+        self.map[pos.0][pos.1].recycler = true;
+        self.map[pos.0][pos.1].can_build = false;
+        self.map[pos.0][pos.1].can_spawn = false;
+        self.m_m -= 10;
+        print!("BUILD {} {};", pos.1, pos.0);
+    }
+
+    fn move_all(&mut self) {
+        while !self.m_units.is_empty() {
+            let u = self.m_units.pop().unwrap();
+            let mut closest: (usize, usize, usize) = (self.w + self.h, 0, 0);
+
+            for x in 0..self.h {
+                for y in 0..self.w {
+                    if self.map[x][y].owner != Owner::Me && self.map[x][y].scrap > 0 {
+                        let dist = self.dist(u, (x, y));
+                        if dist < closest.0 {
+                            closest = (dist, x, y);
+                        }
+                    }
+                }
+            }
+
+            self.r#move(u, (closest.1, closest.2));
+        }
+    }
+
+    fn build_all(&mut self) {
+        while self.m_m >= 10 && self.m_recycler <= self.o_recycler {
+            let mut most_scrap: (Scrap, usize, usize) = (0, 0, 0);
+
+            for x in 0..self.h {
+                for y in 0..self.w {
+                    if self.map[x][y].can_build && self.map[x][y].scrap > most_scrap.0 {
+                        most_scrap = (self.map[x][y].scrap, x, y);
+                    }
+                }
+            }
+
+            self.build((most_scrap.1, most_scrap.2));
+        }
+    }
 }
 
 fn main() {
@@ -122,6 +205,11 @@ fn main() {
 
     loop {
         e.get_input();
-        println!("WAIT");
+
+        e.move_all();
+        e.build_all();
+        // e.spawn_all();
+
+        println!();
     }
 }
