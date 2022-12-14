6c5f5b7f5135877960216b74028724f59e5aa0a2
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-14 11:55:10 +0100

    working

--- codingame/bot/Fall_Challenge_2022.rs
+++ codingame/bot/Fall_Challenge_2022.rs
@@ -1,3 +1,4 @@
+use std::collections::HashSet;
 use std::io;
 
 macro_rules! parse_input {
@@ -138,11 +139,29 @@ impl Env {
         ((src.0 as isize - dst.0 as isize).abs() + (src.1 as isize - dst.1 as isize).abs()) as usize
     }
 
-    fn r#move(&mut self, src: (usize, usize), dst: (usize, usize)) {
-        self.map[dst.0][dst.1].owner = Owner::Me;
-        // can build on top of unit, will need to take care
-        // could free tile to build on
-        print!("MOVE 1 {} {} {} {};", src.1, src.0, dst.1, dst.0);
+    fn next_to_not_owned(&self, pos: (usize, usize)) -> bool {
+        if pos.0 > 0
+            && self.map[pos.0 - 1][pos.1].owner != Owner::Me
+            && self.map[pos.0 - 1][pos.1].scrap > 0
+        {
+            return true;
+        } else if pos.0 < self.h - 1
+            && self.map[pos.0 + 1][pos.1].owner != Owner::Me
+            && self.map[pos.0 + 1][pos.1].scrap > 0
+        {
+            return true;
+        } else if pos.1 > 0
+            && self.map[pos.0][pos.1 - 1].owner != Owner::Me
+            && self.map[pos.0][pos.1 - 1].scrap > 0
+        {
+            return true;
+        } else if pos.1 < self.w - 1
+            && self.map[pos.0][pos.1 + 1].owner != Owner::Me
+            && self.map[pos.0][pos.1 + 1].scrap > 0
+        {
+            return true;
+        }
+        false
     }
 
     fn build(&mut self, pos: (usize, usize)) {
@@ -154,6 +173,48 @@ impl Env {
         print!("BUILD {} {};", pos.1, pos.0);
     }
 
+    fn r#move(&mut self, src: (usize, usize), dst: (usize, usize)) {
+        self.map[dst.0][dst.1].owner = Owner::Me;
+        self.map[src.0][src.1].unit -= 1;
+        if self.map[src.0][src.1].unit == 0 {
+            self.map[src.0][src.1].can_build = true;
+        }
+        self.map[dst.0][dst.1].unit += 1;
+        self.map[dst.0][dst.1].can_build = false;
+        print!("MOVE 1 {} {} {} {};", src.1, src.0, dst.1, dst.0);
+    }
+
+    fn spawn(&mut self, pos: (usize, usize)) {
+        // might put tile at Owner::Me
+        self.map[pos.0][pos.1].unit += 1;
+        self.m_m -= 10;
+        print!("SPAWN 1 {} {};", pos.1, pos.0);
+    }
+
+    fn build_all(&mut self) {
+        if self.o_recycler.len() == 0 {
+            return;
+        }
+
+        while self.m_m >= 10 && self.m_recycler.len() <= self.o_recycler.len() {
+            let mut most_scrap: (Scrap, usize, usize) = (0, 0, 0);
+
+            for x in 0..self.h {
+                for y in 0..self.w {
+                    if self.map[x][y].owner == Owner::Me
+                        && self.map[x][y].can_build
+                        && self.map[x][y].scrap > most_scrap.0
+                        && self.map[x][y].unit == 0
+                    {
+                        most_scrap = (self.map[x][y].scrap, x, y);
+                    }
+                }
+            }
+
+            self.build((most_scrap.1, most_scrap.2));
+        }
+    }
+
     fn move_all(&mut self) {
         while !self.m_units.is_empty() {
             let u = self.m_units.pop().unwrap();
@@ -161,7 +222,10 @@ impl Env {
 
             for x in 0..self.h {
                 for y in 0..self.w {
-                    if self.map[x][y].owner != Owner::Me && self.map[x][y].scrap > 0 {
+                    if self.map[x][y].owner != Owner::Me
+                        && self.map[x][y].scrap > 0
+                        && !self.map[x][y].recycler
+                    {
                         let dist = self.dist(u, (x, y));
                         if dist < closest.0 {
                             closest = (dist, x, y);
@@ -174,19 +238,40 @@ impl Env {
         }
     }
 
-    fn build_all(&mut self) {
-        while self.m_m >= 10 && self.m_recycler <= self.o_recycler {
-            let mut most_scrap: (Scrap, usize, usize) = (0, 0, 0);
+    fn spawn_all(&mut self) {
+        if self.m_m < 10 {
+            return;
+        }
 
-            for x in 0..self.h {
-                for y in 0..self.w {
-                    if self.map[x][y].can_build && self.map[x][y].scrap > most_scrap.0 {
-                        most_scrap = (self.map[x][y].scrap, x, y);
-                    }
+        let mut set: HashSet<(usize, usize)> = HashSet::new();
+        for x in 0..self.h {
+            for y in 0..self.w {
+                if self.map[x][y].owner == Owner::Me
+                    && self.map[x][y].can_spawn
+                    && self.map[x][y].unit == 0
+                    && !self.map[x][y].recycler
+                    && self.next_to_not_owned((x, y))
+                {
+                    set.insert((x, y));
+                }
+            }
+        }
+
+        while self.m_m >= 10 {
+            // empty owned tile closest to center and next to not owned tile with scrap
+            let mut closest: (usize, usize, usize) = (self.w + self.h, 0, 0);
+            for (x, y) in set.iter() {
+                let dist = self.dist((self.h / 2, self.w / 2), (*x, *y));
+                if dist < closest.0 {
+                    closest = (dist, *x, *y);
                 }
             }
 
-            self.build((most_scrap.1, most_scrap.2));
+            if closest.0 == self.w + self.h {
+                break;
+            }
+
+            self.spawn((closest.1, closest.2));
         }
     }
 }
@@ -206,9 +291,9 @@ fn main() {
     loop {
         e.get_input();
 
-        e.move_all();
         e.build_all();
-        // e.spawn_all();
+        e.move_all();
+        e.spawn_all();
 
         println!();
     }
