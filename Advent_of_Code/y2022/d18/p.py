3e338f646e8157d9382b64f5d419f283eb63ab78
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-18 11:09:29 +0100

    d18, black magic

---
+++ Advent_of_Code/y2022/d18/p.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import re
+from collections import namedtuple
+from os.path import dirname
+from typing import FrozenSet, List, Set
+
+from aocd import get_data
+
+DAY = int(re.sub(r'[^0-9]', "", dirname(__file__).split('/')[-1]))
+YEAR = int(re.sub(r'[^0-9]', "", dirname(__file__).split('/')[-2]))
+DATA: str = get_data(day=DAY, year=YEAR)
+
+# DATA = "2,2,2\n1,2,2\n3,2,2\n2,1,2\n2,3,2\n2,2,1\n2,2,3\n2,2,4\n2,2,6\n1,2,5\n3,2,5\n2,1,5\n2,3,5"
+
+lines = DATA.splitlines()
+
+Cube = namedtuple("Cube", "x y z")
+
+cubes: FrozenSet[Cube] = frozenset([Cube(*map(int, l.split(","))) for l in lines])
+
+
+def sides(c: Cube) -> FrozenSet[Cube]:
+    return frozenset(
+        [
+            Cube(c.x + 1, c.y, c.z),
+            Cube(c.x - 1, c.y, c.z),
+            Cube(c.x, c.y + 1, c.z),
+            Cube(c.x, c.y - 1, c.z),
+            Cube(c.x, c.y, c.z + 1),
+            Cube(c.x, c.y, c.z - 1),
+        ]
+    )
+
+
+def count_neighbors(cubes: FrozenSet[Cube], c: Cube) -> int:
+    return sum(1 for n in sides(c) if n in cubes)
+
+
+t = 0
+for c in cubes:
+    t += 6 - count_neighbors(cubes, c)
+
+print(f"part 1:\t{t}")
+
+seen: Set[Cube] = set()
+q: List[Cube] = [Cube(-1, -1, -1)]
+
+while q:
+    cur = q.pop()
+    q += [s for s in (sides(cur) - cubes - seen) if all(-1 <= c <= 25 for c in s)]
+    seen |= {cur}
+
+print(f"part 2:\t{sum((s in seen) for c in cubes for s in sides(c))}")
