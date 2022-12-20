35b34d659640228083f0fb89ab19eeb01b9da802
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-20 12:20:31 +0100

    using chained list

--- Advent_of_Code/y2022/d20/p.py
+++ Advent_of_Code/y2022/d20/p.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
+from __future__ import annotations
+
 import re
 from copy import deepcopy
 from os.path import dirname
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from aocd import get_data
 
@@ -13,97 +15,73 @@ DATA: str = get_data(day=DAY, year=YEAR)
 
 DATA_EXAMPLE = "1\n2\n-3\n3\n-2\n0\n4"
 
-class Num:
-    def __init__(self, n: int, t: int, i: int):
+class Node:
+    def __init__(self, n: int, prev: Optional[Node] = None, next: Optional[Node] = None):
         self.n = n
-        self.t = t
-        self.i = i
-    
-    def update(self, n: int, t: int, i: int):
-        self.n = n
-        self.t = t
-        self.i = i
+        self.prev = prev
+        self.next = next
 
 
-def parse(data: str) -> List[Num]:
+def parse(data: str, key: int = 1) -> List[Node]:
     lines = data.splitlines()
-    sequence: List[Num] = []
+    sequence: List[Node] = [Node(int(l) * key) for l in lines]
 
-    for i, l in enumerate(lines):
-        n = int(l)
-        t = 0
-        for num in sequence:
-            if num.n == n:
-                t += 1
-        sequence.append(Num(n, t, i))
+    for a, b in zip(sequence, sequence[1:]):
+        a.next = b
+        b.prev = a
     
+    sequence[0].prev = sequence[-1]
+    sequence[-1].next = sequence[0]
+
     return sequence
 
 
-def find_num(sequence: List[Num], mix: List[Num], num: Num) -> int:
-    if mix[num.i].n == num.n and mix[num.i].t == num.t:
-        return num.i
+def solve(sequence: List[Node], iteration: int) -> Tuple[int, int, int]:
+    for _ in range(iteration):
+        for node in sequence:
+            node.prev.next = node.next
+            node.next.prev = node.prev
 
-    bi = i = num.i
+            a, b = node.prev, node.next
+            
+            for _ in range(node.n % (len(sequence) - 1)):
+                a = a.next
+                b = b.next
+            
+            a.next = node
+            node.prev = a
+            b.prev = node
+            node.next = b
 
-    while bi >= 0 or i < len(sequence):
-        if bi >= 0:
-            if mix[bi].n == num.n and mix[bi].t == num.t:
-                return bi
-            bi -= 1
-        if i < len(sequence):
-            if mix[i].n == num.n and mix[i].t == num.t:
-                return i
-            i += 1
-    raise Exception("Not found")
-
-
-def solve(sequence: List[Num], mix: List[Num], iteration: int) -> Tuple[int, int, int]:
-    for it in range(iteration):
-        for iv, num in enumerate(sequence):
-            print(f"{it + 1} {iv + 1} / {iteration} {len(sequence)}", end='\r')
-
-            i = find_num(sequence, mix, num)
-            n = deepcopy(mix[i])
+    ret: List[int] = []
+    for node in sequence:
+        if node.n == 0:
+            t = node
+            for _ in range(3):
+                for _ in range(1000):
+                    t = t.next
+                ret.append(t.n)
+            break
     
-            if n.n > 0:
-                for x in range(i, i + n.n):
-                    tmp = mix[(x + 1) % len(mix)]
-                    mix[x % len(mix)].update(tmp.n, tmp.t, x)
-                idx = (i + n.n) % len(mix)
-                mix[idx].update(n.n, n.t, idx)
-            elif n.n < 0:
-                for x in range(i, i + n.n, -1):
-                    tmp = mix[(x - 1) % len(mix)]
-                    mix[x % len(mix)].update(tmp.n, tmp.t, x)
-                idx = (i + n.n) % len(mix)
-                mix[idx].update(n.n, n.t, idx)
-
-    i = find_num(sequence, mix, Num(0, 0, 0))
-    return (mix[(i + 1000) % len(mix)].n,
-            mix[(i + 2000) % len(mix)].n,
-            mix[(i + 3000) % len(mix)].n)
+    return tuple(ret)
+    
 
 
 sequence = parse(DATA_EXAMPLE)
-n = solve(sequence, deepcopy(sequence), 1)
+n = solve(sequence, 1)
 assert n == (4, -3, 2), f"Expected (4, -3, 2), got {n}"
 assert sum(n) == 3, f"Expected 3, got {sum(n)}"
 
 sequence = parse(DATA)
-n = solve(sequence, deepcopy(sequence), 1)
-print(f"\npart 1:\t{sum(n)}")
+n = solve(sequence, 1)
+print(f"part 1:\t{sum(n)}")
 
 KEY = 811589153
-sequence = parse(DATA_EXAMPLE)
-for num in sequence:
-    num.n *= KEY
-n = solve(sequence, deepcopy(sequence), 10)
-assert n == (811589153, 2434767459, 1623178306), f"Expected (811589153, 2434767459, 1623178306), got {n}"
+sequence = parse(DATA_EXAMPLE, KEY)
+n = solve(sequence, 10)
+assert n == (811589153, 2434767459, -1623178306), f"Expected (811589153, 2434767459, 1623178306), got {n}"
 assert sum(n) == 1623178306, f"Expected 1623178306, got {sum(n)}"
 
-sequence = parse(DATA)
-for num in sequence:
-    num.n *= KEY
-n = solve(sequence, deepcopy(sequence), 10)
-print(f"\npart 2:\t{sum(n)}")
+sequence = parse(DATA, KEY)
+n = solve(sequence, 10)
+print(f"part 2:\t{sum(n)}")
