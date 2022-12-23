d40d7085a703c6c0547927b23fe5eb32efc18071
Alexdelia <alexandre.delille.57@gmail.com>
2022-12-23 10:49:22 +0100

    stop

--- Advent_of_Code/y2022/d23/p.py
+++ Advent_of_Code/y2022/d23/p.py
@@ -26,17 +26,18 @@ DATA_EXAMPLE = """\
 """
 
 SIZE = 1000
-GRID = List[List[str]]
+GRID = List[List[bool]]
 
 
 def parse(data: str) -> GRID:
-    grid = [['.' for _ in range(SIZE)] for _ in range(SIZE)]
+    grid: GRID = [[False for _ in range(SIZE)] for _ in range(SIZE)]
 
     x = (SIZE - len(data.splitlines())) // 2
     for l in data.splitlines():
         y = (SIZE - len(data.splitlines()[0])) // 2
         for c in l:
-            grid[x][y] = c
+            if c == '#':
+                grid[x][y] = True
             y += 1
         x += 1
 
@@ -44,13 +45,13 @@ def parse(data: str) -> GRID:
 
 
 def decide(grid: GRID, x: int, y: int) -> Tuple[int, int]:
-    if grid[x - 1][y - 1] == '.' and grid[x - 1][y] == '.' and grid[x - 1][y + 1] == '.':
+    if not grid[x - 1][y - 1] and not grid[x - 1][y] and not grid[x - 1][y + 1]:
         return (x - 1, y)
-    elif grid[x + 1][y - 1] == '.' and grid[x + 1][y] == '.' and grid[x + 1][y + 1] == '.':
+    elif not grid[x + 1][y - 1] and not grid[x + 1][y] and not grid[x + 1][y + 1]:
         return (x + 1, y)
-    elif grid[x - 1][y - 1] == '.' and grid[x][y - 1] == '.' and grid[x + 1][y - 1] == '.':
+    elif not grid[x - 1][y - 1] and not grid[x][y - 1] and not grid[x + 1][y - 1]:
         return (x, y - 1)
-    elif grid[x - 1][y + 1] == '.' and grid[x][y + 1] == '.' and grid[x + 1][y + 1] == '.':
+    elif not grid[x - 1][y + 1] and not grid[x][y + 1] and not grid[x + 1][y + 1]:
         return (x, y + 1)
     else:
         return (x, y)
@@ -58,13 +59,13 @@ def decide(grid: GRID, x: int, y: int) -> Tuple[int, int]:
 
 def simulate(g: GRID, round: int) -> GRID:
     for _ in range(round):
-        gp: GRID = [['.' for _ in range(SIZE)] for _ in range(SIZE)]
+        gp: GRID = [[False for _ in range(SIZE)] for _ in range(SIZE)]
 
         for x in range(SIZE):
             for y in range(SIZE):
-                if g[x][y] == '#':
+                if g[x][y]:
                     nx, ny = decide(g, x, y)
-                    gp[nx][ny] = '#'
+                    gp[nx][ny] = True
         
         g = gp
     
@@ -76,17 +77,24 @@ def calc(g: GRID) -> int:
     x1, x2, y1, y2 = SIZE, 0, SIZE, 0
     for x in range(SIZE):
         for y in range(SIZE):
-            if g[x][y] == '#':
+            if g[x][y]:
                 x1, x2 = min(x1, x), max(x2, x)
                 y1, y2 = min(y1, y), max(y2, y)
 
+    s = ""
     for x in range(x1 - 3, x2 + 1 + 3):
-        print(''.join(g[x][y1 - 3:y2 + 1 + 3]))
+        for y in range(y1 - 3, y2 + 1 + 3):
+            if g[x][y]:
+                s += '#'
+            else:
+                s += '.'
+        s += '\n'
+    print(s)
 
     t = 0    
     for x in range(x1, x2 + 1):
         for y in range(y1, y2 + 1):
-            if g[x][y] == '.':
+            if not g[x][y]:
                 t += 1
     return t
 
