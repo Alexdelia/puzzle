d6fc05c35a6c8ccde39b897e601d3630addd118b
Alexdelia <alexandre.delille.57@gmail.com>
2023-01-09 14:24:35 +0100

    update

--- codingame/optimize/2048/src/answer.py
+++ codingame/optimize/2048/src/answer.py
@@ -1,8 +1,28 @@
-import math
-import sys
-
 SIZE = 4
 CHUNK = 9000
+ENCODE = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:;<=>?@[]^_`{|}~"
+DECODE = "UDLR"
+
+
+def btob10(n: str, key: str) -> int:
+    ret: int = 0
+    l: int = len(key)
+
+    for i, c in enumerate(n[::-1]):
+        ret += key.index(c) * (l ** i)
+
+    return ret
+
+
+def b10tob(n: int, key: str) -> str:
+    ret: str = ""
+    l: int = len(key)
+
+    while n > 0:
+        n, r = divmod(n, l)
+        ret += key[r]
+
+    return ret[::-1]
 
 
 def get_input():
@@ -13,12 +33,13 @@ def get_input():
         grid.append(input().split())
     return seed, score, grid
 
+
 answer = {}
 
 seed, _, _ = get_input()
 
 if seed in answer:
-    m = answer[seed]
+    m = b10tob(btob10(answer[seed], ENCODE), DECODE)
     for i in range(0, len(m), CHUNK):
         print(m[i:i + CHUNK])
         get_input()
