5eed6c30333cfb59cb534d82c7a5aa2d161ff912
Alexdelia <alexandre.delille.57@gmail.com>
2023-01-10 18:25:49 +0100

    `57:Square_root_convergents.py`

---
+++ euler/57:Square_root_convergents.py
@@ -0,0 +1,18 @@
+N = 1000
+
+n = d = 1
+np = dp = 10
+
+t = 0
+for k in range(N):
+    n, d = n + 2 * d, n + d
+
+    if n >= np:
+        np *= 10
+    if d >= dp:
+        dp *= 10
+
+    if np > dp:
+        t += 1
+
+print(t)
