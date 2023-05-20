2d5a705f93809691529bc05502a53be1cb878321
adelille / Alexdelia <60811847+Alexdelia@users.noreply.github.com>
2023-05-20 12:07:39 +0200

    `2D_map.py`

---
+++ sololearn/2D_map.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class Coord:
+    x: int
+    y: int
+
+def manhattan_distance(c1: Coord, c2: Coord) -> int:
+    return abs(c1.x - c2.x) + abs(c1.y - c2.y)
+
+p: list[Coord] = []
+
+x = 0
+y = 0
+
+for c in input():
+    if c == 'P':
+        p.append(Coord(x, y))
+
+    if c == ',':
+        x = 0
+        y += 1
+    else:
+        x += 1
+
+print(manhattan_distance(p[0], p[1]))
