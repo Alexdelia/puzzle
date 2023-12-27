fdb4393bf6f76094b36bdbef065e4e67a3603a74
Alexdelia <alexandre.delille.57@gmail.com>
2023-12-27 17:58:03 +0100

    Matrix dot multiplication

--- codingame/bot/Fall_Challenge_2023/src/matrix.rs
+++ codingame/bot/Fall_Challenge_2023/src/matrix.rs
@@ -28,6 +28,32 @@ impl<'a, 'b> Add<&'b Matrix> for &'a Matrix {
         }
     }
 }
+
+impl Matrix {
+    pub fn dot(&self, other: &Matrix) -> Matrix {
+        assert_eq!(self.col, other.row);
+
+        let mut data = Vec::with_capacity(self.row * other.col);
+
+        for x in 0..self.row {
+            for y in 0..other.col {
+                let mut sum = 0.0;
+
+                for z in 0..self.col {
+                    sum += self.data[x * self.col + z] * other.data[z * other.col + y];
+                }
+
+                data.push(sum);
+            }
+        }
+
+        Matrix {
+            row: self.row,
+            col: other.col,
+            data,
+        }
+    }
+}
 // ANSWER END
 
 #[cfg(test)]
@@ -52,5 +78,22 @@ mod tests {
 
         let d = &a + &c;
         assert_eq!(d.data, vec![7.0, 10.0, 13.0, 16.0]);
+    }
+
+    #[test]
+    fn test_dot() {
+        let a = Matrix {
+            row: 2,
+            col: 3,
+            data: vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+        };
+        let b = Matrix {
+            row: 3,
+            col: 2,
+            data: vec![7.0, 8.0, 9.0, 10.0, 11.0, 12.0],
+        };
+
+        let c = a.dot(&b);
+        assert_eq!(c.data, vec![58.0, 64.0, 139.0, 154.0]);
     }
 }
