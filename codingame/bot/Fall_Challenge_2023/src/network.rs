7d35d84450f0460a75bed8df07cdb986138c199d
Alexdelia <alexandre.delille.57@gmail.com>
2023-12-30 01:09:49 +0100

    forward propagation

--- codingame/bot/Fall_Challenge_2023/src/network.rs
+++ codingame/bot/Fall_Challenge_2023/src/network.rs
@@ -1,7 +1,19 @@
-use crate::Layer;
+use crate::{Float, Layer};
 
 // ANSWER START
 pub struct Network {
     pub layers: Vec<Layer>,
+}
+
+impl Network {
+    pub fn forward(&self, input: Vec<Float>) -> Vec<Float> {
+        let mut output = input;
+
+        for layer in &self.layers {
+            output = layer.process(&output);
+        }
+
+        output
+    }
 }
 // ANSWER END
