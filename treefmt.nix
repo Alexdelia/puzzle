830e758469055d402587b45388e7ed51425f3408
Alexdelia <alexandre.delille.57@gmail.com>
2025-04-04 22:14:06 +0200

    `nix flake`

---
+++ treefmt.nix
@@ -0,0 +1,29 @@
+_: {
+  projectRootFile = ".git/config";
+
+  programs = {
+    rustfmt = {
+      enable = true;
+      edition = "2024";
+    };
+
+    alejandra.enable = true;
+    deadnix.enable = true;
+    statix.enable = true;
+  };
+
+  settings.global.excludes = [
+    ".gitingore"
+
+    "*.lock"
+
+    ".env*"
+
+    "*.png"
+    "*.ico"
+
+    "*.toml"
+
+    "code_golf/"
+  ];
+}
