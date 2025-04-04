f31f95a03b924624943ca2235864f175b16c850c
Alexdelia <alexandre.delille.57@gmail.com>
2025-04-05 00:08:07 +0200

    save test on new cool idea

--- codingame/optimize/Spring_Challenge_2025.rs
+++ codingame/optimize/Spring_Challenge_2025.rs
@@ -3,16 +3,19 @@
 use std::collections::VecDeque;
 
 type Depth = u8;
-type Board = u32;
+
+type BoardBitSize = u32;
+struct Board(BoardBitSize);
+type BoardIndex = u8;
+
 type Sum = u32;
 
-const DICE_MAX: Board = 6;
+const DICE_MAX: BoardBitSize = 6;
 
 const SUM_MOD: Sum = 2 ^ 30;
 
 fn main() {
 	let (depth, starting_board) = parse();
-	dbg!(depth, starting_board);
 
 	println!("{sum}", sum = solve(depth, starting_board));
 }
@@ -28,54 +31,61 @@ fn parse() -> (Depth, Board) {
 	std::io::stdin().read_line(&mut input_line).unwrap();
 	let depth = parse_input!(input_line, u8);
 
-	let mut starting_board: u32 = 0;
-
-	for _ in 0..3_usize {
-		let mut inputs = String::with_capacity(6);
-		std::io::stdin().read_line(&mut inputs).unwrap();
-		for x in inputs.split_whitespace() {
-			starting_board *= 10;
-			starting_board += parse_input!(x, u32);
-		}
-	}
+	let starting_board = Board::parse();
 
 	(depth, starting_board)
 }
 
-// cells
-const C_BR: Board = 000_000_001;
-const C_B_: Board = 000_000_010;
-const C_BL: Board = 000_000_100;
-const C_R_: Board = 000_001_000;
-const C_M_: Board = 000_010_000;
-const C_L_: Board = 000_100_000;
-const C_TR: Board = 001_000_000;
-const C_T_: Board = 010_000_000;
-const C_TL: Board = 100_000_000;
+impl Board {
+	fn parse() -> Self {
+		let mut board = Self(0);
 
-macro_rules! cell {
-	($b:expr, $c:expr) => {
-		(($b / $c) % 10)
-	};
-}
+		for i in 0..3 {
+			let mut inputs = String::with_capacity(6);
+			std::io::stdin().read_line(&mut inputs).unwrap();
+			let line = inputs.split_whitespace().collect::<Vec<_>>();
 
-macro_rules! raw_cell {
-	($b:expr, $c:expr) => {
-		((($b / $c) % 10) * $c)
-	};
+			let shift = i * 3;
+			board.set(shift, parse_input!(line[0], BoardBitSize));
+			board.set(shift + 1, parse_input!(line[1], BoardBitSize));
+			board.set(shift + 2, parse_input!(line[2], BoardBitSize));
+		}
+
+		board
+	}
+
+	pub fn set(&mut self, index: BoardIndex, value: BoardBitSize) {
+		let shift = index * 3;
+		let mask = !(0b111 << shift);
+		self.0 = (self.0 & mask) | (value << shift);
+	}
+
+	pub fn get(&self, index: BoardIndex) -> BoardBitSize {
+		(self.0 >> (index * 3)) & 0b111
+	}
+
+	const C_BR: BoardIndex = 0;
+	const C_B_: BoardIndex = 1;
+	const C_BL: BoardIndex = 2;
+	const C_R_: BoardIndex = 3;
+	const C_M_: BoardIndex = 4;
+	const C_L_: BoardIndex = 5;
+	const C_TR: BoardIndex = 6;
+	const C_T_: BoardIndex = 7;
+	const C_TL: BoardIndex = 8;
 }
 
 macro_rules! play_move {
     ($board:ident, $cell:ident, $depth:ident, $queue:ident, $($neighbors:ident),+) => {
-        if cell!($board, $cell) == 0 {
-            let n = 0 $(+ cell!($board, $neighbors))+;
+        if cell_value!($board, $cell) == 0 {
+            let n = 0 $(+ cell_value!($board, $neighbors))+;
             if n == 0 || n > DICE_MAX {
                 $queue.push_back(($board + $cell, $depth));
             } else {
                 $queue.push_back((
                     $board
-                        $( - raw_cell!($board, $neighbors) )+
-                        + $cell,
+                        $( - raw_cell_value!($board, $neighbors) )+
+                        + ($cell * n),
                     $depth,
                 ));
             }
@@ -106,8 +116,27 @@ fn solve(depth: Depth, starting_board: Board) -> Sum {
 		play_move!(board, C_T_, d, queue, C_TL, C_TR, C_M_);
 		play_move!(board, C_TL, d, queue, C_T_, C_L_);
 
-		dbg!(d, queue.len());
+		dbg!(d, queue.len(), board);
 	}
 
 	sum
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_get() {
+		let board = Board(0b_001_010_011_100_101_110_000_010_100);
+		assert_eq!(board.get(Board::C_BR), 1);
+		assert_eq!(board.get(Board::C_B_), 2);
+		assert_eq!(board.get(Board::C_BL), 3);
+		assert_eq!(board.get(Board::C_R_), 4);
+		assert_eq!(board.get(Board::C_M_), 5);
+		assert_eq!(board.get(Board::C_L_), 6);
+		assert_eq!(board.get(Board::C_TR), 0);
+		assert_eq!(board.get(Board::C_T_), 2);
+		assert_eq!(board.get(Board::C_TL), 4);
+	}
 }
