2cca8c8c122f230b64453c8d1443e697a93d27ab
Alexdelia <alexandre.delille.57@gmail.com>
2025-05-03 23:42:23 +0200

    shorter

--- codingame/code_golf/Temperature.c
+++ codingame/code_golf/Temperature.c
@@ -1,1 +1,1 @@
-main(){int n,c,t;scanf("%d",&n);c=n*999;while(n--)scanf("%d",&t),c=abs(t)<abs(c)?t:c,c=0<t&t==-c?t:c;printf("%d",c);}
+main(n,c,t){scanf("%d",&n);c=n<<9;while(n--)scanf("%d",&t),c=abs(t)<abs(c)?t:c,c=0<t&t==-c?t:c;printf("%d",c);}
