91b6a41123b31d6d4b7687b317834904b67764d2
Alexdelia <alexandre.delille.57@gmail.com>
2025-05-03 23:52:48 +0200

    golfing

--- codingame/code_golf/Power_of_Thor.c
+++ codingame/code_golf/Power_of_Thor.c
@@ -1,1 +1,1 @@
-main(){int x,y,t,T;scanf("%d%d%d%d",&x,&y,&t,&T);for(;;)printf("%s%s\n",y<T?T--,"N":y>T?T++,"S":"",x>t?t++,"E":x<t?t--,"W":"");}
+main(x,y,t,T){scanf("%d%d%d%d",&x,&y,&t,&T);for(;;)printf("%s%s\n",y<T?T--,"N":y>T?T++,"S":"",x>t?t++,"E":x<t?t--,"W":"");}
