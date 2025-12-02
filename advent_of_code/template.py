07ad9430dd430301cedbfab79c3fa98cf06a7d63
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-02 22:43:22 +0100

    feat: trying out new template

--- advent_of_code/template.py
+++ advent_of_code/template.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 
-import re
-from os.path import dirname
+import sys
 
-from aocd import get_data
+sys.path.append("../..")
+from get_data import get_data
 
-DAY = int(re.sub(r"[^0-9]", "", dirname(__file__).split("/")[-1]))
-YEAR = int(re.sub(r"[^0-9]", "", dirname(__file__).split("/")[-2]))
-DATA: str = get_data(day=DAY, year=YEAR)
+DATA: str = get_data()
 
 lines = DATA.splitlines()
 
