f4a4c9af3b079f20617924d08aebae8c720b60ad
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-05 21:32:52 +0100

    feat: `aoc` `2025` `05` `p2`

--- advent_of_code/y2025/d05/p.py
+++ advent_of_code/y2025/d05/p.py
@@ -12,7 +12,26 @@ range_chunk, item_chunk = DATA.split("\n\n")
 ranges = [tuple(map(int, line.split("-"))) for line in range_chunk.splitlines()]
 items = [int(line) for line in item_chunk.splitlines()]
 
+
+def merge_ranges(ranges: list[tuple[int, int]]) -> list[tuple[int, int]]:
+	ranges = sorted(ranges, key=lambda x: x[0])
+	merged = [ranges.pop(0)]
+
+	for r in ranges:
+		if r[0] <= merged[-1][1]:
+			merged[-1] = (merged[-1][0], max(r[1], merged[-1][1]))
+			continue
+
+		merged.append(r)
+
+	return merged
+
+
+ranges = merge_ranges(ranges)
+
+
 p1 = 0
+p2 = 0
 
 for r in ranges:
 	for i in range(len(items) - 1, -1, -1):
@@ -20,10 +39,7 @@ for r in ranges:
 			p1 += 1
 			items.pop(i)
 
+	p2 += r[1] - r[0] + 1
+
 print(f"part 1:\t{p1}")
-
-
-p2 = 0
-
-
 print(f"part 2:\t{p2}")
