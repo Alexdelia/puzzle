3bc56e0df049b1b1077c6305f20facedb7407754
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-06 09:52:00 +0100

    feat: failed `aoc` `2025` `06` `p2`

--- advent_of_code/y2025/d06/p.py
+++ advent_of_code/y2025/d06/p.py
@@ -1,33 +1,47 @@
 #!/usr/bin/env python3
 
+import math
 import sys
+
+
+def row_num(row: str) -> list[int]:
+	return [int(n) for n in row.split()]
 
 
 def solve(data: str) -> tuple[int, int]:
 	lines = list(reversed(data.splitlines()))
 
 	sign_row = lines[0].split()
+	numbers = lines[1:]
 
-	res_row = [int(n) for n in lines[1].split()]
+	p1_row = row_num(numbers[0])
+	p2_col = []
 
-	for line in lines[2:]:
-		number_row = [int(n) for n in line.split()]
-		for i, n in enumerate(number_row):
-			if sign_row[i] == "+":
-				res_row[i] += n
-			elif sign_row[i] == "*":
-				res_row[i] *= n
+	for x, line in enumerate(numbers):
+		number_row = row_num(line)
 
-	return (sum(res_row), 0)
+		if x > 0:
+			for y, n in enumerate(number_row):
+				if sign_row[y] == "+":
+					p1_row[y] += n
+				elif sign_row[y] == "*":
+					p1_row[y] *= n
+
+		if sign_row[x] == "+":
+			p2_col.append(sum(number_row))
+		elif sign_row[x] == "*":
+			p2_col.append(math.prod(number_row))
+
+	return (sum(p1_row), sum(p2_col))
 
 
 test = """\
-123 328  51 64 
- 45 64  387 23 
+123 328  51 64
+ 45 64  387 23
   6 98  215 314
 *   +   *   +
 """
-expected = (4277556, 0)
+expected = (4277556, 3263827)
 got = solve(test)
 assert expected[0] == got[0], (
 	f"part 1 test failed: expected {expected[0]}, got {got[0]}"
