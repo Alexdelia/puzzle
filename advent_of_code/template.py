30e077caf7eb724b9db6f83be9b1d55045bb8004
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-06 17:56:10 +0100

    feat: improve template

--- advent_of_code/template.py
+++ advent_of_code/template.py
@@ -2,11 +2,30 @@
 
 import sys
 
+
+def solve(data: str) -> tuple[int, int]:
+	lines = data.splitlines()
+
+	print(lines)
+
+	return (0, 0)
+
+
+test = """\
+"""
+expected = (0, 0)
+got = solve(test)
+assert expected[0] == got[0], (
+	f"part 1 test failed: expected {expected[0]}, got {got[0]}"
+)
+assert expected[1] == got[1], (
+	f"part 2 test failed: expected {expected[1]}, got {got[1]}"
+)
+
+
 sys.path.append("../..")
 from get_data import get_data
 
-DATA: str = get_data()
-
-lines = DATA.splitlines()
-
-print(lines)
+p1, p2 = solve(get_data())
+print(f"part 1:\t{p1}")
+print(f"part 2:\t{p2}")
