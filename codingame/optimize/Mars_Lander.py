0d280567713c67417d747b207ee0b1e52e52d5e3
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-07 17:21:41 +0100

    chore: code quality `codingame`

--- codingame/optimize/Mars_Lander.py
+++ codingame/optimize/Mars_Lander.py
@@ -4,10 +4,12 @@
 land: list[tuple[int, int]] = []
 
 n = int(input())  # the number of points used to draw the surface of Mars.
-for i in range(n):
-	# land_x: X coordinate of a surface point. (0 to 6999)
-	# land_y: Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
-	land.append(tuple(map(int, input().split())))
+
+# land_x: X coordinate of a surface point. (0 to 6999)
+# land_y: Y coordinate of a surface point.
+# By linking all the points together in a sequential fashion,
+# you form the surface of Mars.
+land.extend(tuple(map(int, input().split())) for _ in range(n))
 
 flat_x: tuple[int, int] = (0, 0)
 flat_y: int = 0
