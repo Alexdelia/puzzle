b7fca161087f9947b27444492ba8d529f74392d8
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-07 11:02:44 +0100

    feat: `aoc` `2015` `12` `p2`

--- advent_of_code/y2015/d12/p.py
+++ advent_of_code/y2015/d12/p.py
@@ -2,17 +2,40 @@
 
 import re
 import sys
+import json
+
+
+def parse_obj(obj: dict[str, any] | list[any] | int | str) -> int:
+	if isinstance(obj, int):
+		return obj
+
+	if isinstance(obj, list):
+		return sum(parse_obj(item) for item in obj)
+
+	if isinstance(obj, dict):
+		if "red" in obj.values():
+			return 0
+
+		return sum(parse_obj(value) for value in obj.values())
+
+	return 0
 
 
 def solve(data: str) -> tuple[int, int]:
-	regex = re.compile(r"(\-?\d+)")
+	number_regex = re.compile(r"\-?\d+")
 
 	p1 = 0
 
-	for match in regex.findall(data):
+	for match in number_regex.findall(data):
 		p1 += int(match)
 
-	return (p1, 0)
+	# === part 2 ===
+
+	data = json.loads(data)
+
+	p2 = parse_obj(data)
+
+	return (p1, p2)
 
 
 sys.path.append("../..")
