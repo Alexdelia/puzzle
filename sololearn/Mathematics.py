f8af01511c71e145a8933e0d20469387e9819e36
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-07 16:44:53 +0100

    chore: code quality `sololearn/Mathematics.py`

--- sololearn/Mathematics.py
+++ sololearn/Mathematics.py
@@ -3,11 +3,11 @@ import re
 
 def expression_index(n: int, ex: list[str]) -> int | None:
 	for i, e in enumerate(ex):
-		if n == eval(e):
+		if n == eval(e):  # noqa: S307
 			return i
 	return None
 
 
 r = expression_index(int(input()), re.sub(r"[()]", " ", input()).split())
 
-print(["none", f"index {r}"][r != None])
+print(["none", f"index {r}"][r is not None])
