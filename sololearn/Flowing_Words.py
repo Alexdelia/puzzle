4880230236cc773ec5b742fc89a857d90b13f459
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-07 16:43:12 +0100

    chore: code quality `sololearn/Flowing_Words.py`

--- sololearn/Flowing_Words.py
+++ sololearn/Flowing_Words.py
@@ -1,8 +1,5 @@
 def is_flowing(words: list[str]) -> bool:
-	for i in range(len(words) - 1):
-		if words[i][-1] != words[i + 1][0]:
-			return False
-	return True
+	return all(words[i][-1] == words[i + 1][0] for i in range(len(words) - 1))
 
 
 print(str(is_flowing(input().split())).lower())
