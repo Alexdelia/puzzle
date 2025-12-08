84c6b2fcb95dba6a90424f2d0e587e51013f6f22
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-08 20:57:44 +0100

    feat: parsing `2025` `08`

--- advent_of_code/y2025/d08/point.rs
+++ advent_of_code/y2025/d08/point.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Point {
 	pub x: PointUnit,
@@ -6,3 +8,45 @@ pub struct Point {
 }
 
 pub type PointUnit = u32;
+
+impl FromStr for Point {
+	type Err = String;
+
+	fn from_str(s: &str) -> Result<Self, Self::Err> {
+		let coords: Vec<&str> = s.trim().split(',').collect();
+		if coords.len() != 3 {
+			return Err(format!("Invalid point format: {s}"));
+		}
+
+		Ok(Point {
+			x: coords[0]
+				.parse::<PointUnit>()
+				.map_err(|e| format!("Invalid x coordinate: {e}"))?,
+			y: coords[1]
+				.parse::<PointUnit>()
+				.map_err(|e| format!("Invalid y coordinate: {e}"))?,
+			z: coords[2]
+				.parse::<PointUnit>()
+				.map_err(|e| format!("Invalid z coordinate: {e}"))?,
+		})
+	}
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_parse_point() {
+		let point_str = "162,817,812";
+		let point = point_str.parse::<Point>();
+		assert_eq!(
+			point,
+			Ok(Point {
+				x: 162,
+				y: 817,
+				z: 812
+			})
+		);
+	}
+}
