006ba46485aece67902e77bf30b6196534c7b842
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-12 23:09:18 +0100

    feat: another idea `2025` `10` `p2`

--- advent_of_code/y2025/d10/node.rs
+++ advent_of_code/y2025/d10/node.rs
@@ -1,13 +1,16 @@
 use crate::Distance;
 
+type Priority = u16;
+
 pub struct Node<T> {
 	pub t: T,
 	pub dist: Distance,
+	pub priority: Priority,
 }
 
 impl<T> PartialEq for Node<T> {
 	fn eq(&self, other: &Self) -> bool {
-		self.dist.eq(&other.dist)
+		self.dist == other.dist && self.priority == other.priority
 	}
 }
 
@@ -15,13 +18,15 @@ impl<T> Eq for Node<T> {}
 
 impl<T> PartialOrd for Node<T> {
 	fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-		Some(other.dist.cmp(&self.dist))
+		Some(self.cmp(other))
 	}
 }
 
 impl<T> Ord for Node<T> {
 	fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-		other.dist.cmp(&self.dist)
+		self.priority
+			.cmp(&other.priority)
+			.then_with(|| other.dist.cmp(&self.dist))
 	}
 }
 
@@ -36,16 +41,56 @@ mod tests {
 	#[test]
 	fn test_node_ordering() {
 		let mut heap = BinaryHeap::<Node<State>>::from([
-			Node { t: 0b0000, dist: 5 },
+			Node {
+				t: 0b0000,
+				dist: 5,
+				priority: 0,
+			},
 			Node {
 				t: 0b0000,
 				dist: 10,
+				priority: 0,
 			},
-			Node { t: 0b0000, dist: 3 },
+			Node {
+				t: 0b0000,
+				dist: 3,
+				priority: 0,
+			},
 		]);
 
 		assert_eq!(heap.pop().unwrap().dist, 3);
 		assert_eq!(heap.pop().unwrap().dist, 5);
 		assert_eq!(heap.pop().unwrap().dist, 10);
+	}
+
+	#[test]
+	fn test_node_ordering_with_priority() {
+		let mut heap = BinaryHeap::<Node<State>>::from([
+			Node {
+				t: 0b0000,
+				dist: 5,
+				priority: 2,
+			},
+			Node {
+				t: 0b0000,
+				dist: 5,
+				priority: 1,
+			},
+			Node {
+				t: 0b0000,
+				dist: 3,
+				priority: 0,
+			},
+		]);
+
+		let first = heap.pop().unwrap();
+		assert_eq!(first.dist, 5);
+		assert_eq!(first.priority, 2);
+		let second = heap.pop().unwrap();
+		assert_eq!(second.dist, 5);
+		assert_eq!(second.priority, 1);
+		let third = heap.pop().unwrap();
+		assert_eq!(third.dist, 3);
+		assert_eq!(third.priority, 0);
 	}
 }
