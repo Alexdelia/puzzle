872d9550476a349651ddab2c6a45bbc8c0b90cda
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-21 15:43:07 +0100

    feat: trying external solver

---
+++ codingame/optimize/Travelling_Salesman.concorde/solver.rs
@@ -0,0 +1,42 @@
+use std::io;
+
+macro_rules! parse_input {
+	($x:expr, $t:ident) => {
+		$x.trim().parse::<$t>().unwrap()
+	};
+}
+
+fn main() {
+	let mut input_line = String::new();
+	io::stdin().read_line(&mut input_line).unwrap();
+	let n = parse_input!(input_line, i32);
+
+	let mut input = Vec::with_capacity(n as usize);
+
+	for _ in 0..n as usize {
+		let mut input_line = String::new();
+		io::stdin().read_line(&mut input_line).unwrap();
+		let inputs = input_line.split(" ").collect::<Vec<_>>();
+		let x = parse_input!(inputs[0], i32);
+		let y = parse_input!(inputs[1], i32);
+
+		input.push((x, y));
+	}
+
+	let names = input
+		.iter()
+		.cloned()
+		.enumerate()
+		.map(|(i, _)| i.to_string())
+		.collect::<Vec<_>>();
+
+	let c = elkai_rs::Coordinates2D::new(std::collections::HashMap::from_iter(
+		input
+			.iter()
+			.cloned()
+			.enumerate()
+			.map(|(i, c)| (names[i].as_str(), c)),
+	));
+
+	println!("{s}", s = c.solve(10).join(" "));
+}
