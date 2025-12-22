fb5fd3c331d1281b86660711cbc6f26bd416307e
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-22 22:52:04 +0100

    feat: cooler solution

--- codingame/optimize/CGFunge_Prime.groovy
+++ codingame/optimize/CGFunge_Prime.groovy
@@ -1,28 +1,34 @@
 def s = """\
-v    >"EMIRP TON"CCCCCCCCCE 
->DD2-:"EMIRP"CCCCCE
-     D
-     D
-     2
-     /
-     2
-     *
-     -
- >v3D:
- XD  >"EMIRP TON"CCCCCCCCCE 
- 3D
- DD
- D4
- XX
- 11
- +X
- 2/
->:D"EMIRP TON"CCCCCCCCCE
-- 2
-X X
-2 -
-^*:"EMIRP"CCCCCE
-^*<
+v      >"EMIRP TON"CCCCCCCCCE
+D  >D2-:"EMIRP"CCCCCE
+>3-:"EMIRP"CCCCCE
+   >DD2/2*-:"EMIRP TON"CCCCCCCCCE
+           D
+           D
+           3
+           /                       E
+           3                       C
+           *                       C
+           -                       C
+           >:"EMIRP TON"CCCCCCCCCE C
+            D                      C
+            D                      C
+            D                      C
+            D                      C
+E           5          >"EMIRP"CCCCCE|
+C           >DDDD*4X1X-:"EMIRP TON"^
+C           +
+C           4          4
+C           X          X
+C           5          1
+C           D          X
+C           D          /
+C           D          *
+C           D          3
+^"NOT PRIME":-X2DX2*/X1X4D<
+                       -  D
+                       :2+^
+  ECCCCCCCCC"NOT PRIME"<
 """
 println s.count('\n')
 print s
