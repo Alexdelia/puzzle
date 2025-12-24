f43075866bbfa18b5440c27fc60cf92b6c9bef81
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-24 20:27:46 +0100

    feat: better landscape visualization

--- codingame/optimize/Mars_Lander/src/visualize.rs
+++ codingame/optimize/Mars_Lander/src/visualize.rs
@@ -2,6 +2,12 @@ use svg::Document;
 
 use crate::{referee::env::Axis, segment::Segment};
 
+const BACKGROUND_COLOR: &str = "black";
+
+const LANDSCAPE_BORDER_COLOR: &str = "lightgray";
+const LANDSCAPE_FILL_COLOR: &str = "gray";
+const LANDING_SEGMENT_COLOR: &str = "green";
+
 const MAX_WIDTH: Axis = 7000.0;
 const MAX_HEIGHT: Axis = 3000.0;
 
@@ -25,14 +31,59 @@ pub fn landscape(segment_list: &[Segment]) -> Document {
 		.set("width", SVG_WIDTH)
 		.set("height", SVG_HEIGHT);
 
+	document = document.add(
+		svg::node::element::Rectangle::new()
+			.set("x", 0)
+			.set("y", 0)
+			.set("width", SVG_WIDTH)
+			.set("height", SVG_HEIGHT)
+			.set("fill", BACKGROUND_COLOR),
+	);
+
+	let mut point_list = Vec::with_capacity(segment_list.len() + 2);
+	point_list.push((0.0, MAX_HEIGHT * CONVERSION_HEIGHT));
 	for segment in segment_list {
+		point_list.push((
+			segment.a.x * CONVERSION_WIDTH,
+			SVG_HEIGHT as f64 - segment.a.y * CONVERSION_HEIGHT,
+		));
+	}
+	point_list.push((
+		segment_list.last().unwrap().b.x * CONVERSION_WIDTH,
+		SVG_HEIGHT as f64 - segment_list.last().unwrap().b.y * CONVERSION_HEIGHT,
+	));
+	point_list.push((MAX_WIDTH * CONVERSION_WIDTH, MAX_HEIGHT * CONVERSION_HEIGHT));
+
+	document = document.add(
+		svg::node::element::Polygon::new()
+			.set(
+				"points",
+				point_list
+					.iter()
+					.map(|(x, y)| format!("{x},{y}"))
+					.collect::<Vec<String>>()
+					.join(" "),
+			)
+			.set("fill", LANDSCAPE_FILL_COLOR),
+	);
+
+	for segment in segment_list {
+		let is_landing = segment.a.y == segment.b.y;
+
 		let line = svg::node::element::Line::new()
 			.set("x1", segment.a.x * CONVERSION_WIDTH)
 			.set("y1", SVG_HEIGHT as f64 - segment.a.y * CONVERSION_HEIGHT)
 			.set("x2", segment.b.x * CONVERSION_WIDTH)
 			.set("y2", SVG_HEIGHT as f64 - segment.b.y * CONVERSION_HEIGHT)
-			.set("stroke", "gray")
-			.set("stroke-width", 2);
+			.set(
+				"stroke",
+				if is_landing {
+					LANDING_SEGMENT_COLOR
+				} else {
+					LANDSCAPE_BORDER_COLOR
+				},
+			)
+			.set("stroke-width", 3);
 		document = document.add(line);
 	}
 
