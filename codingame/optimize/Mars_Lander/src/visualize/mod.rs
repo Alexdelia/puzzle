c89e19f3aaabc574d92de1ba0d49920b35f80fa7
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-25 22:33:54 +0100

    chore: output fs quality

--- codingame/optimize/Mars_Lander/src/visualize/mod.rs
+++ codingame/optimize/Mars_Lander/src/visualize/mod.rs
@@ -3,9 +3,12 @@ pub use landscape::landscape;
 mod solution;
 pub use solution::solution;
 
-use crate::referee::env::{Axis, MAX_HEIGHT, MAX_WIDTH};
+use crate::{
+	OUTPUT_DIR,
+	referee::env::{Axis, MAX_HEIGHT, MAX_WIDTH},
+};
 
-const OUTPUT_DIR: &str = "output/visualize";
+const VISUALIZE_OUTPUT_DIR: &str = "visualize";
 
 const SVG_WIDTH: i32 = 1000;
 const SVG_HEIGHT: i32 = (SVG_WIDTH as f64 * MAX_HEIGHT / MAX_WIDTH) as i32;
@@ -14,7 +17,9 @@ const CONVERSION_WIDTH: f64 = SVG_WIDTH as f64 / MAX_WIDTH;
 const CONVERSION_HEIGHT: f64 = SVG_HEIGHT as f64 / MAX_HEIGHT;
 
 pub fn write_doc(validator_name: &str, doc: &svg::Document, iteration: usize) {
-	let output_dir = std::path::Path::new(OUTPUT_DIR).join(validator_name);
+	let output_dir = std::path::Path::new(OUTPUT_DIR)
+		.join(validator_name)
+		.join(VISUALIZE_OUTPUT_DIR);
 	if !output_dir.exists() {
 		std::fs::create_dir_all(&output_dir).unwrap_or_else(|e| {
 			panic!("failed to create output directory {output_dir:?}: {e}");
