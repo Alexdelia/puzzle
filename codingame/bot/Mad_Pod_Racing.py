911800d87df99329866fd1f65b75821bd007b14c
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-28 16:54:39 +0100

    chore: remove dead comment

--- codingame/bot/Mad_Pod_Racing.py
+++ codingame/bot/Mad_Pod_Racing.py
@@ -2,9 +2,6 @@ import math
 import sys
 from typing import Any, ClassVar
 
-# Auto-generated code below aims at helping you parse
-# the standard input according to the problem statement.
-
 POD_RADIUS = 400
 
 T_ANGLE = 15
