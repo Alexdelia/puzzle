382c1f06d54ca5ff91ae9c25890510d566451fa5
Alexdelia <alexandre.delille.57@gmail.com>
2025-12-28 12:43:02 +0100

    feat: improve CG

--- codingame/code_golf/Don't Panic - Episode 1.py
+++ codingame/code_golf/Don't Panic - Episode 1.py
@@ -1,3 +1,5 @@
-no=input;v=int;n,_,_,F,P,_,_,N=o().split();a=[v(P)]*v(n);N=v(N)
-while N:f,p=o().split();a[v(f)]=v(p);N-=1
-while 1:f,p,d=o().split();f=v(f);p=v(p);print(['WAIT','BLOCK'][f>=0and(a[f]>p)==(d[0]=='L')and a[f]!=p])
+o=lambda:input().split()
+v=int
+n,_,_,F,P,_,_,N=map(v,o());a=[P]*n
+while N:f,p=o();a[v(f)]=v(p);N-=1
+while 1:f,p,d=o();f,p=v(f),v(p);print(['WAIT','BLOCK'][f>=0and(a[f]>p)==(d[0]=='L')and a[f]!=p])
