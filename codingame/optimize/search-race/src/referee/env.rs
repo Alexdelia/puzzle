31044a1c0493549ffe9fcfb35348fcaaad703aa7
Alexdelia <alexandre.delille.57@gmail.com>
2026-03-22 13:39:02 +0100

    chore: code quality

--- codingame/optimize/search-race/src/referee/env.rs
+++ codingame/optimize/search-race/src/referee/env.rs
@@ -13,7 +13,9 @@ pub type Degree = i16;
 
 pub const FRICTION: f64 = 0.85;
 
+#[cfg(feature = "visualize")]
 pub const MAX_WIDTH: Axis = 16000.0;
+#[cfg(feature = "visualize")]
 pub const MAX_HEIGHT: Axis = 9000.0;
 
 pub const CHECKPOINT_RADIUS: Axis = 600.0;
